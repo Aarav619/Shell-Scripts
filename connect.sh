ae06fec7b611aadf4f443e1f59cb45d791aee385
Aarav619 <aaravmishra619@gmail.com>
2022-02-09 01:21:13 +0530

    Modify script
    Removed hardcoded device names in notifications

--- connect.sh
+++ connect.sh
@@ -5,19 +5,24 @@ device1="6C:0D:E1:88:ED:93"
 device2="00:1E:7C:92:E6:FC"
 
 
+if [ "$1" == "-help" ]; then
+    echo -e "This script attempts to connect or disconnects to paired Bluetooth device."
+    exit 0;
+fi
+
+
 if bluetoothctl info "$device1" | grep 'Connected: yes' -q; then
-  bluetoothctl disconnect "$device1" && notify-send "Disconnected from Oppo Enco Buds!"
-  
+  bluetoothctl disconnect "$device1" && info="$(bluetoothctl info ${device1} | grep Name | cut -d' ' -f2-)" && notify-send "Disconnected from ${info}!"
 else
-  bluetoothctl connect "$device1" && notify-send "Connected to Oppo Enco Buds!"
+  bluetoothctl connect "$device1" && info="$(bluetoothctl info ${device1} | grep Name | cut -d' ' -f2-)" && notify-send "Connected to $info!"
 fi
 
 
 if bluetoothctl info "$device2" | grep 'Connected: yes' -q; then
-  bluetoothctl disconnect "$device2" && notify-send "Disconnected from Infinity Glide 510!"
+  bluetoothctl disconnect "$device2" && info="$(bluetoothctl info $device2 | grep Name | cut -d' ' -f2-)" && notify-send "Disconnected from $info!"
   
 else
-  bluetoothctl connect "$device2" && notify-send "Connected to Infinity Glide 510!"
+  bluetoothctl connect "$device2" && info="$(bluetoothctl info ${device2} | grep Name | cut -d' ' -f2-)" && notify-send "Connected to $info!"
 fi
 
 
