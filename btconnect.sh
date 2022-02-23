876e96b2a2516e12bbb3c3a24cffe00ff20094a3
Aarav619 <aaravmishra619@gmail.com>
2022-02-24 00:15:28 +0530

    fix: info alert; upon connection

--- btconnect.sh
+++ btconnect.sh
@@ -9,12 +9,12 @@ fi
 
 var="$(bluetoothctl paired-devices | grep '' | cut -d' ' -f3- | nl -w2 -s': ' | fzf --prompt "bluetooth ")"
 
-index="$(echo $var | grep '' | cut -d ':' -f1)"
+index="$(echo $var | grep '' | cut -d':' -f1)"
 
 device="$(bluetoothctl paired-devices | grep '' | cut -d' ' -f2- | awk '{print $1}' | sed -n ${index}p)"
 
 if bluetoothctl info "$device" | grep 'Connected: yes' -q; then
-  bluetoothctl disconnect "$device" && info="$(bluetoothctl info ${device} | grep Name | cut -d' ' -f2-)" && notify-send "Disconnected from ${info}!"
+  bluetoothctl disconnect "$device" && info="$(bluetoothctl info ${device} | grep Name | cut -d':' -f2)" && notify-send "Disconnected from ${info}!"
 else
-  bluetoothctl connect "$device" && info="$(bluetoothctl info ${device} | grep Name | cut -d' ' -f2-)" && notify-send "Connected to ${info}!"
+  bluetoothctl connect "$device" && info="$(bluetoothctl info ${device} | grep Name | cut -d':' -f2)" && notify-send "Connected to ${info}!"
 fi
