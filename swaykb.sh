50f04811f4209011a22960c9a6e8a49bf56d17ec
aaaravv <aaravmishra619@gmail.com>
2023-01-08 21:06:19 +0530

    chore: Remove scripts

--- swaykb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#!/usr/bin/env bash
-sed -n '/Key bindings/,/Ctrl+Shift+r/p' ~/.config/sway/config | \
-grep '#\|##\|bindsym\|^$' | \
-sed -e 's/# //' -e 's/#//' -e 's/##//' -e 's/###//' -e 's/^[ \t]*//' | \
-grep -v '#Key bindings' | \
-yad --text-info --fore=#46d9ff --geometry=1200x800
-
-
